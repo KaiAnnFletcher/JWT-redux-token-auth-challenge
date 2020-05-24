c2efce9ef83f9740051b2639a1cbec1c9dd082db
Kai <kaiann.fletcher@mail.utoronto.ca>
2020-05-24 01:30:04 -0400

    Fixed error with registration not registering new users.

--- routes/api/users.js
+++ routes/api/users.js
@@ -25,7 +25,7 @@ router.post("/register", (req, res) => {
         return res.status(400).json(errors);
     }
 
-    User.findOne({ email: req.body }).then(user => {
+    User.findOne({ where: {email: req.body.email} }).then(user => {
         if (user) {
             return res.status(400).json({ email: "Email already exists" });
         } else {
--- validation/login.js
+++ validation/login.js
@@ -11,9 +11,9 @@ module.exports = function validateLoginInput(data) {
 
     //Email checks
     if(Validator.isEmpty(data.email)) {
-        errors.email = "Username field is required";
+        errors.email = "Email field is required";
     } else if (!Validator.isEmail(data.email)) {
-        errors.email = "Username is invalid";
+        errors.email = "Email is invalid";
     }
 
     //Password checks
